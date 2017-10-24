a42eee74ccb8cb5b070cc97a54ab55e01b489a8d
Alexander <asigachev84@gmail.com>
2017-10-24 19:24:44 +0300

    Update Mountains.py
    Добавлено: Исходная таблица, отфильтрованная так, что в ней остаются только горы, относящиеся к хребтам, к которым относится более 3 гор

--- Mountains.py
+++ Mountains.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('Mountains.csv')
+# Таблица с названиями хребтов, к которым относится более 3 гор
 df.groupby(['Parent mountain']).size().reset_index(name='Count').query('Count>3')
+
+# Исходная таблица, отфильтрованная так, что в ней остаются только горы, относящиеся к хребтам, к которым относится более 3 гор
+df.groupby(['Parent mountain']).filter(lambda x: len(x) > 3)
